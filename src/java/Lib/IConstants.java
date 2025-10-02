73a71bca45b3d740d874b534791e5003f43957d7
Yuki <162554358+NgDai@users.noreply.github.com>
2025-10-02 14:17:29 +0700

    First commit

---
+++ src/java/Lib/IConstants.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lib;
+
+/**
+ *
+ * @author Nguyễn Đại
+ */
+public interface IConstants {
+    final String AC_SEARCH="search";
+    final String AC_CREATEACC="create account";
+    final String AC_LOGIN="login";
+    final String AC_DEFAULT="default";
+    
+    //define hằng số ứng với tên file
+    final String SEARCH_CONTROLLER = "SearchController";
+    final String CREATE_CONTROLLER = "CreateAccountController";
+    final String LOGIN_CONTROLLER = "LoginPage.html";
+    final String DEFAULT_PAGE = "index.html";
+}
